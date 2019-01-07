880296cbed56dfa8d57fff44bb48c03f08decf45
zyskiu <zyskiu@163.com>
2019-01-07 23:58:04 +0800

    ok

---
+++ springmvc-le/src/main/java/springmvc/le/Demo.java
@@ -0,0 +1,10 @@
+package springmvc.le;
+
+/**
+ * @author Isen
+ * @date 2019/1/7 23:16
+ * @since 1.0
+ */
+public class Demo {
+
+}
---
+++ springmvc-le/src/main/java/springmvc/le/controller/GoController.java
@@ -0,0 +1,33 @@
+package springmvc.le.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Isen
+ * @date 2019/1/7 23:16
+ * @since 1.0
+ */
+@Controller
+public class GoController {
+    private final Log logger = LogFactory.getLog(GoController.class);
+
+    /**
+     * HEAD请求适合检查服务器状态，不会返回body，节省网络资源
+     */
+    @RequestMapping(value = {"/"}, method = {RequestMethod.HEAD})
+    public String head(){
+        return "go.jsp";
+    }
+
+    @RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
+    public String index(Model model) throws Exception{
+        logger.info("========index=========");
+        model.addAttribute("msg", "Go!");
+        return "go.jsp";
+    }
+}
